1a607c76d770eead02621b45b9facaadb29b3e61
Cameron <ccproinsights@gmail.com>
2025-09-24 14:18:46 -0700

    fixed import

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
-from .app import create_app
-from .config import ProductionConfig
+from app import create_app
+from config import ProductionConfig
 
 # Pass the ProductionConfig class directly to the factory
 app = create_app(ProductionConfig)
