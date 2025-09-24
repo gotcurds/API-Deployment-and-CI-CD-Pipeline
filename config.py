e2bfaa91b2a853b4816af116a81bb2d206eb9e89
Cameron <ccproinsights@gmail.com>
2025-09-24 14:23:49 -0700

    more imports fixed

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 import os
-from dotenv import load_dotenv
-
-# This will load the variables from your .env file
-load_dotenv()
 
 class ProductionConfig:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     # Add any other configuration variables here
-    
+
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
     DEBUG = True
